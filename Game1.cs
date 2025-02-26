3ee3abf20b3c52aaa39be93014c4086c49714d58
Joaquim-Renato <jocajoaquimas@gmail.com>
2025-02-26 15:17:14 -0300

    Adicionar arquivos de projeto.

---
+++ Game1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace corrida
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        // elementos graficos do jogo
+        private Texture2D _carro1;
+        private Texture2D _carro2;
+        private Texture2D _cenario3;
+        private Vector2 _posicaoCarro1;
+        private Vector2 _posicaoCarro2;
+
+        // questões
+        private string _equacao;
+        private int _resultado;
+        private Random _random;
+
+        //estado do teclado 
+        private KeyboardState _estadoTecladoAnterior;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new corrida.Game1();
+game.Run();
